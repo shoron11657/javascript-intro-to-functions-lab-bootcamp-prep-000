c4d71becab217efe9ea27adcb074b30b6d62f82f
Kevin M <shoron@gmail.com>
2017-11-09 04:57:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function shout(string) {  return string.toUpperCase()}
 function whisper(X) {return X.toLowerCase()}
 function logShout(hello) {console.log(`${hello}`.toUpperCase())}
 function logWhisper(HELLO) {console.log(`${HELLO}`.toLowerCase())}
-function sayHiToGrandma(string) {
-    if(string===string.toLowerCase()){return `I can't hear you!`}
-    else if(string===string.toUpperCase()){return `YES INDEED!`}
-    else if(string==='I love you, Grandma.'){return 'I love you, too.'}
+function sayHiToGrandma(x5) {
+    if(x5===x5.toLowerCase()){return `I can't hear you!`}
+    else if(x5===x5.toUpperCase()){return `YES INDEED!`}
+    else if(x5==='I love you, Grandma.'){return `I love you, too.`}
   }
